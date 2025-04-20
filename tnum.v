adaa98ed4d3d16fc8d758ad3081c4afe18774dae
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2025-04-20 13:16:42 +0530

    use modules

--- tnum.v
+++ tnum.v
@@ -8,29 +8,39 @@ Variant bit := zero | one.
 
 (* TODO switch to 64 after testing *)
 Definition SIZE := 4.
+Definition v64 := Vector.t bit SIZE.
 
-Section tnum.
+Module tnum.
   (* Type tnum reflects the Kernel tnum, which is a record consisting of
    * v, the value bits, and m, the mask bits (Greek mu).
    *)
-
-  Definition tnum_v := Vector.t bit SIZE.
-  Definition tnum_m := Vector.t bit SIZE.
-  Variant tnum := tcons : tnum_v -> tnum_m -> tnum.
+  Variant t := cons (v : v64) (m : v64) : t.
 End tnum.
 
-Section vtnum.
+Module otnum.
   (* The Kernel representation technically allows four values per trit,
    * but forbids the combination {.v = 1, .m = 1} by convention (i.e.,
    * the value bit should be reset if the mask is set). The representation
-   * we use here is a true tristate one.
+   * we use here is a true tristate one, ruling out illegal constructions
+   * in any intermediate steps.
    *)
   
-  Definition vtnum := Vector.t (option bit) SIZE.
-  Definition vtnil := Vector.nil (option bit).
-  Definition vtcons := Vector.cons (option bit).
-  Arguments vtcons _ {_} _.
+  Definition t := Vector.t (option bit) SIZE.
+  Definition nil := Vector.nil (option bit).
+  Definition cons := Vector.cons (option bit).
+  Arguments cons _ {_} _.
 
-  Example vt10 : vtnum := (vtcons (Some one) (vtcons (Some zero) (vtcons (Some one) (vtcons (Some zero) vtnil)))).
-  Example vt3 : vtnum := (vtcons (Some zero) (vtcons (Some zero) (vtcons (Some one) (vtcons (Some one) vtnil)))).
-End vtnum.
+  Example vt10 := (cons (Some one) (cons (Some zero) (cons (Some one) (cons (Some zero) nil)))).
+  Example vt3  := (cons (Some zero) (cons (Some zero) (cons (Some one) (cons (Some one) nil)))).
+End otnum.
+
+(* TODO REM; wrote down just as a reference for the usage of nth; main point: I do not have to generate proof for `i < SIZE`. *)
+(* TODO take v.length instead of SIZE *)
+Goal forall (v : otnum.t) i (hidx : i < SIZE), let ith := Vector.nth v (Fin.of_nat_lt hidx) in ith = Some one \/ ith = Some zero \/ ith = None.
+  intros v i hidx.
+  destruct (Vector.nth v (Fin.of_nat_lt hidx)).
+  destruct b; auto.
+  auto.
+Qed.
+
+(* TODO from Harisankar et al. *)
