2a2577fdf28ac86c640b0efec638e5452f917d67
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2025-09-24 03:20:29 +0530

    start custom Vector.t using list and sig

---
+++ sigVector.v
@@ -0,0 +1,33 @@
+(* 2025-09-24 *)
+
+From Stdlib Require Import List.
+From Stdlib Require Import Program.
+
+Lemma nth_no_error {A} {i}:
+  forall x : list A, forall {n}, length x = n -> i < n -> nth_error x i <> None.
+Proof.
+  induction i.
+  - destruct x; destruct n; easy.
+  - destruct n. easy. rewrite nth_error_S.
+    destruct x. simpl. easy.
+    simpl. intros hs1 hs2.
+    assert (length x = n). auto.
+    assert (i < n). apply PeanoNat.lt_S_n; assumption.
+    apply (IHi x n); assumption.
+Qed.
+
+Module Vector.
+  Definition t A n := { x : list A | length x = n }.
+  Definition projlist {A} {n} (x : t A n) := proj1_sig x.
+  Definition projhlen {A} {n} (x : t A n) := proj2_sig x.
+
+  Program Definition nth_order {A} {n} (v : t A n) {i} (hi : i < n) :=
+    match (List.nth_error (projlist v) i) with
+    | None => !
+    | Some x => x
+    end.
+  Next Obligation.
+    destruct v.
+    apply (nth_no_error x e hi). auto.
+  Defined.
+End Vector.
